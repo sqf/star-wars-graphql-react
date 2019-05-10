8a1175b85dacadff063ddb0b2ba54220ca595c9a
Samir Senhadri <sqf@o2.pl>
2019-05-10 13:00:30 +0200

    Pagination part 1

--- src/copmonents/AllPlanets.js
+++ src/copmonents/AllPlanets.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
 import {ApolloProvider, Query} from "react-apollo";
@@ -8,9 +8,13 @@ const client = new ApolloClient({
     uri: "https://swapi.apis.guru/\n"
 });
 
+const FIRST = 10;
+
 function AllPlanets() {
     const [shoudShowPlanetDetails, setShoudShowPlanetDetails] = useState(false);
     const [selectedPlanetId, setSelectedPlanetId] = useState(null);
+    const [paginationState, setPaginationState] = useState({startCursor: 10, endCursor: null});
+
     function handlePlanetClick() {
         setSelectedPlanetId(this.planetId);
         setShoudShowPlanetDetails(true);
@@ -19,9 +23,14 @@ function AllPlanets() {
     const Planets = () => (
         <Query
             query={gql`
-      {
-        allPlanets {
-          totalCount
+      query AllPlanets($cursor: String) {
+        allPlanets(first: 11, after: $cursor) {
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+            startCursor
+            endCursor
+          }
           planets {
             id
             name
@@ -39,17 +48,31 @@ function AllPlanets() {
           }
         }
       }
-    `}>
+    `}
+     variables={{ cursor: null }}>
             {({loading, error, data}) => {
                 if (loading) return <tr><td>Loading...</td></tr>;
                 if (error) return <tr><td>Error :(</td></tr>;
 
-                return data.allPlanets.planets.map(({id, name, gravity}) => (
+                let getPaginationLinksStyle = (isAvailable) => {
+                    return {
+                        opacity: isAvailable ? 1 : 0.4
+                    }
+                };
+
+                const shouldUpdatePaginationState =
+                    (paginationState.startCursor !== data.allPlanets.pageInfo.startCursor) ||
+                        (paginationState.endCursor !== data.allPlanets.pageInfo.endCursor);
+
+                if (shouldUpdatePaginationState)
+                    setPaginationState({startCursor: data.allPlanets.pageInfo.startCursor, endCursor: data.allPlanets.pageInfo.endCursor});
+
+                return [...data.allPlanets.planets.map(({id, name, gravity}) => (
                     <tr key={id}>
                         <td onClick={handlePlanetClick.bind({planetId: id})}>{name}</td>
                         <td>{gravity}</td>
                     </tr>
-                ));
+                )), (<tr key={'pagination'} ><td style={getPaginationLinksStyle(data.allPlanets.pageInfo.hasPreviousPage)}>Previous</td><td style={getPaginationLinksStyle(data.allPlanets.pageInfo.hasNextPage)}>Next</td></tr>)];
             }}
         </Query>
     );
@@ -66,7 +89,7 @@ function AllPlanets() {
                         </tr>
                         </thead>
                         <tbody>
-                            <Planets/>
+                            <Planets />
                         </tbody>
                     </table>
                 </header>
