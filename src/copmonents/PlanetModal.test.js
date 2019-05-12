5b765f00b2624efa7b68aa8c3c286e6f81910505
Samir Senhadri <sqf@o2.pl>
2019-05-12 15:48:12 +0200

    Finished tests for planet attributes in modal

--- src/copmonents/PlanetModal.test.js
+++ src/copmonents/PlanetModal.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import {configure, shallow, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { MockedProvider } from 'react-apollo/test-utils';
+import {MockedProvider} from 'react-apollo/test-utils';
 
 import Modal from 'react-modal';
 
 import PlanetModal from './PlanetModal';
-import { GET_PLANET_QUERY } from './PlanetModal';
+import {GET_PLANET_QUERY} from './PlanetModal';
 
 configure({adapter: new Adapter()});
 
@@ -23,6 +23,11 @@ const SOME_FAKE_PLANET_TERRAINS = ['jungle', 'rainforests'];
 const SOME_FAKE_PLANET_SURFACE_WATER = 8;
 const SOME_FAKE_PLANET_RESIDENTS = [{name: 'Luke Skywalker'}, {name: 'C-3PO'}, {name: 'Darth Vader'}];
 const SOME_FAKE_PLANET_FILMS = [{title: 'A New Hope'}, {title: 'Return of the Jedi'}, {title: 'The Phantom Menace'}];
+
+const EXPECTED_CLIMATES_OUTPUT = 'temperate, tropical';
+const EXPECTED_TERRAINS_OUTPUT = 'jungle, rainforests';
+const EXPECTED_RESIDENTS_OUTPUT = 'Luke Skywalker, C-3PO, Darth Vader';
+const EXPECTED_FILMS_OUTPUT = 'A New Hope, Return of the Jedi, The Phantom Menace';
 
 describe('<PlanetModal />', () => {
 
@@ -65,21 +70,99 @@ describe('<PlanetModal />', () => {
     ];
 
     it('should render modal', () => {
-        const wrapper = shallow(<PlanetModal shouldBeVisible={true} setShoudShowPlanetDetails={() => {}}/>);
+        const wrapper = shallow(<PlanetModal shouldBeVisible={true} setShoudShowPlanetDetails={() => {
+        }}/>);
         expect(wrapper.find(Modal)).toHaveLength(1);
     });
 
-    it('should render correct name when query succeds', () => {
+    describe('Planet attributes', () => {
         const wrapper = mount(
             <MockedProvider mocks={mocks} addTypename={false}>
-                <PlanetModal shouldBeVisible={true} setShoudShowPlanetDetails={() => {}} selectedPlanetId={SOME_FAKE_PLANET_ID} />
+                <PlanetModal shouldBeVisible={true} setShoudShowPlanetDetails={() => {
+                }} selectedPlanetId={SOME_FAKE_PLANET_ID}/>
             </MockedProvider>
         );
-        // expect(wrapper.find(Modal)).toHaveLength(1);
-        return executeMockProviderTestCase(wrapper).then(() => {
-            const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
-            expect(rows[11]).toEqual(SOME_FAKE_PLANET_NAME);
-            expect(wrapper.find("table").length).toEqual(1);
+
+        it('should render a table inside modal', () => {
+            expect(wrapper.find('table')).toHaveLength(1);
         });
+
+        it('should render correct name value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[11]).toEqual(SOME_FAKE_PLANET_NAME);
+            });
+        });
+
+        it('should render correct diameter value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[12]).toEqual(SOME_FAKE_PLANET_DIAMETER.toString());
+            });
+        });
+
+        it('should render correct rotation period value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[13]).toEqual(SOME_FAKE_PLANET_ROTATION_PERIOD.toString());
+            });
+        });
+
+        it('should render correct orbital period value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[14]).toEqual(SOME_FAKE_PLANET_ORBITAL_PERIOD.toString());
+            });
+        });
+
+        it('should render correct gravity value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[15]).toEqual(SOME_FAKE_PLANET_GRAVITY);
+            });
+        });
+
+        it('should render correct population value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[16]).toEqual(SOME_FAKE_PLANET_POPULATION.toString());
+            });
+        });
+
+        it('should render correct climates value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[17]).toEqual(EXPECTED_CLIMATES_OUTPUT);
+            });
+        });
+
+        it('should render correct terrains value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[18]).toEqual(EXPECTED_TERRAINS_OUTPUT);
+            });
+        });
+
+        it('should render correct surface water value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[19]).toEqual(SOME_FAKE_PLANET_SURFACE_WATER.toString());
+            });
+        });
+
+        it('should render correct planet residents value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[20]).toEqual(EXPECTED_RESIDENTS_OUTPUT);
+            });
+        });
+
+        it('should render correct planet films value when query succeeds', () => {
+            return executeMockProviderTestCase(wrapper).then(() => {
+                const rows = wrapper.find('tbody').find('tr').find('td').map(column => column.text());
+                expect(rows[21]).toEqual(EXPECTED_FILMS_OUTPUT);
+            });
+        });
+
     });
 });
